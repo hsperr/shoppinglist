d95a35924618349febef2132870f8caf31b34244
Dan Merritt <djosme@gmail.com>
2015-05-15 01:49:48 +0900

    Camera upgrades
    Enumerate and select cameras
    Enumerate and select optimum preview size
    Select preview rotation angle based on sensor orientation and angle to "natural" rotation
    Check hardware capabilities for flash and autofocus
    Refactor preview surface code/callback structure

--- app/src/main/java/shoppinglist/com/shoppinglist/camera/CameraActivity.java
+++ app/src/main/java/shoppinglist/com/shoppinglist/camera/CameraActivity.java
@@ -1,26 +1,23 @@
 package shoppinglist.com.shoppinglist.camera;
 
-import android.graphics.Bitmap;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Surface;
-import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
-
-import java.io.IOException;
-import java.util.List;
 
 import shoppinglist.com.shoppinglist.R;
 
 public class CameraActivity extends ActionBarActivity implements View.OnClickListener, Camera.ShutterCallback {
 
-    private ImageButton mShutter;
-    private CameraPreviewSurfaceView mSurface;
+    private CameraPreviewSurface mSurface;
     private Camera mCamera;
     private Vibrator mVibrator; // LOL!
 
@@ -30,42 +27,62 @@ public class CameraActivity extends ActionBarActivity implements View.OnClickLis
         setContentView(R.layout.activity_camera);
 
         mVibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
-        mSurface = (CameraPreviewSurfaceView)findViewById(R.id.surface);
-        mShutter = (ImageButton) findViewById(R.id.shutter);
+        ImageButton shutter = (ImageButton) findViewById(R.id.shutter);
+        shutter.setOnClickListener(this);
 
-        mShutter.setOnClickListener(this);
+        mSurface = new CameraPreviewSurface(this);
+        ViewGroup surfaceContainer = (ViewGroup)findViewById(R.id.surface_container);
+        surfaceContainer.removeAllViews();
+        surfaceContainer.addView(mSurface);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mCamera = Camera.open();
-        mCamera.autoFocus(new Camera.AutoFocusCallback() {
-            @Override
-            public void onAutoFocus(boolean success, Camera camera) {
-                if (success) {
-                    mVibrator.vibrate(50);
-                }
-            }
-        });
 
-        mSurface.setCamera(mCamera);
-        mSurface.startPreview();
+        releaseCameraAndPreview();
+        mCamera = CameraUtils.selectCamera(this, Camera.CameraInfo.CAMERA_FACING_BACK);
+        if (mCamera != null) {
+            if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_AUTOFOCUS)) {
+                mCamera.autoFocus(new Camera.AutoFocusCallback() {
+                    @Override
+                    public void onAutoFocus(boolean success, Camera camera) {
+                        if (success) {
+                            mVibrator.vibrate(50);
+                        }
+                    }
+                });
+            }
+
+            mSurface.setCamera(mCamera);
+        } else {
+            new AlertDialog.Builder(this)
+                .setMessage(R.string.error_camera_acquisition)
+                .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        return;
+                    }
+                })
+                .create()
+                .show();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mCamera != null) {
-            // Shoudn't need to check, but maybe some nasty app didn't release
-            mSurface.stopPreview();
-            mCamera.release();
-        }
+        releaseCameraAndPreview();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_camera, menu);
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+            menu.removeItem(R.id.action_torch);
+        }
         return true;
     }
 
@@ -84,6 +101,22 @@ public class CameraActivity extends ActionBarActivity implements View.OnClickLis
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+        mCamera.takePicture(this, null, new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                // TODO Data is in JPEG format, save it somewhere and do stuff with it
+                // TODO TODO can't assume that camera supports jpeg
+            }
+        });
+    }
+
+    @Override
+    public void onShutter() {
+        finish();
+    }
+
     private void toggleTorch() {
         Camera.Parameters params = mCamera.getParameters();
         if (params.getFlashMode().equals(Camera.Parameters.FLASH_MODE_TORCH)) {
@@ -94,18 +127,11 @@ public class CameraActivity extends ActionBarActivity implements View.OnClickLis
         mCamera.setParameters(params);
     }
 
-    @Override
-    public void onClick(View v) {
-        mCamera.takePicture(this, null, new Camera.PictureCallback() {
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-                // TODO Data is in JPEG format, save it somewhere and do stuff with it
-            }
-        });
-    }
-
-    @Override
-    public void onShutter() {
-        finish();
+    private void releaseCameraAndPreview() {
+        mSurface.setCamera(null);
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
 }
