b378718349778eb1ae43d8f77c6286e1e276c4b8
hsperr <henning.sperr@gmail.com>
2016-03-29 07:12:49 +0900

    ENH: reverse sort order

--- app/src/main/java/shoppinglist/com/shoppinglist/ItemComparator.java
+++ app/src/main/java/shoppinglist/com/shoppinglist/ItemComparator.java
@@ -7,6 +7,6 @@ public class ItemComparator implements java.util.Comparator<ShoppingItem> {
     @Override
     public int compare(ShoppingItem lhs, ShoppingItem rhs) {
         //a negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater than the second.
-        return lhs.createdAt().compareTo(rhs.createdAt());
+        return -lhs.createdAt().compareTo(rhs.createdAt());
     }
 }
